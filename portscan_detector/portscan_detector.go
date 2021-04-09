c38970aaa4298027d824f7bbd5e9fd6ca1d2e306
Kyle Mistele <kyle@mistele.com>
2021-04-08 23:29:06 -0500

    modify the scan detection to ignore ephemeral ports

--- portscan_detector/portscan_detector.go
+++ portscan_detector/portscan_detector.go
@@ -7,19 +7,23 @@ import (
 	"github.com/google/gopacket/pcap"
 	. "github.com/k-mistele/pcap_portscan_detector/set"
 	"strings"
+	"strconv"
 )
 
+const reasonableDifferentPortThreshhold = 10
+const lowestEphemeralPort = 32768 // IANA SUGGESTS 49152, BUT LOTS OF LINUX KERNELS USE 32768
+
 // DETERMINE IF A TCP STREAM IS A SCAN
-func isScanStream( stream *TCPStream) bool {
+func isScanStream(stream *TCPStream) bool {
 
 	// IF HTTP/S IS THE SOURCE, PROBABLY NOT A PORT SCAN SINCE THAT'D COME FROM AN EPHEMERAL QUIRK.
 	// THIS TRIES TO AVOID A QUIRK WITH THE PROTOCOL
-	if strings.Contains(stream.SrcPort, "http"){
+	if strings.Contains(stream.SrcPort, "http") {
 		return false
 	}
 
 	// LOOK FOR MALFORMED TCP CONNECTION SETUP AND TEARDOWN
-	if !stream.HasSYN  || !stream.HasACK ||!stream.HasFIN {
+	if !stream.HasSYN || !stream.HasACK || !stream.HasFIN {
 		return true
 	}
 
@@ -72,11 +76,11 @@ func buildTCPStreams(packetSource *gopacket.PacketSource) (*map[gopacket.Flow]*T
 // IDENTIFY ATTACKER AND VICTIM HOSTS AND PORTS
 func identifyTargets(tcpStreams *map[gopacket.Flow]*TCPStream) (attackingHosts Set, victimHosts Set, victimPorts Set, victimPortMap *map[string][]string) {
 
-	m := make(map[string] []string)
+	m := make(map[string][]string)
 	victimPortMap = &m
 
 	// LOOP ACROSS TCP STREAMS
-	for flow := range (*tcpStreams) {
+	for flow := range *tcpStreams {
 
 		// IDENTIFY LIKELY SCAN STREAMS
 		stream := (*tcpStreams)[flow]
@@ -110,17 +114,18 @@ func identifyTargets(tcpStreams *map[gopacket.Flow]*TCPStream) (attackingHosts S
 }
 
 // CORRELATE EACH HOST WITH A LIST OF TCP STREAMS THAT IT IS THE SOURCE OF
-func correlateStreamsToHosts(tcpStreams *map[gopacket.Flow] *TCPStream) (*map[string] []*TCPStream, error) {
+func correlateStreamsToHosts(tcpStreams *map[gopacket.Flow]*TCPStream) (*map[string][]*TCPStream, error) {
 
 	// CREATE MAP TO CORRELATE ORIGINATING IP TO STREAMS
-	m := make(map[string] []*TCPStream)
+	m := make(map[string][]*TCPStream)
 
 	// LOOP ACROSS STREAMS AND ASSIGN THEM TO MAP
 	for flow := range *tcpStreams {
 		stream := (*tcpStreams)[flow]
 
 		// CHECK TO SEE IF THE SRC HOST HAS AN ENTRY IN THE MAP
-		_, exists := m[stream.SrcHost]; if exists {
+		_, exists := m[stream.SrcHost]
+		if exists {
 
 			// IF THERE'S AN ENTRY IN THE MAP, ADD THE STREAM TO THE LIST OF STREAMS
 			m[stream.SrcHost] = append(m[stream.SrcHost], stream)
@@ -133,6 +138,79 @@ func correlateStreamsToHosts(tcpStreams *map[gopacket.Flow] *TCPStream) (*map[st
 	}
 
 	return &m, nil
+}
+
+// IDENTIFY CONNECT SCANS
+func identifyScans(tcpStreamsBySrcHost *map[string][]*TCPStream) (*[]string, *map[string]*Set) {
+
+	var attackingHosts []string
+	targetHostToPortMap := make(map[string]*Set)
+
+	// IDENTIFY ATTACKS BY ID A HOST IS THE SOURCE OF CONNECTIONS TO > 10 DIFFERENT PORT NUMBERS IN A SHORT AMOUNT OF TIME
+	// THIS WOULD PROBABLY BE A TCP CONNECT SCAN
+	// LOOP ACROSS STREAMS BY SRC HOST
+	for srcHost := range *tcpStreamsBySrcHost {
+		streams := (*tcpStreamsBySrcHost)[srcHost]
+
+		// BUILD A LIST OF TARGET PORTS FOR EACH SRC HOST
+		portNumbers := Set{}
+		for _, stream := range streams {
+
+			// MAKE SURE WE AREN'T ADDING EPHEMERAL PORTS SINCE THOSE MAY BE USED BY HTTP CONNECTIONS AND FTP
+
+			// PARSE THE STRING INTO AN INT, CUTTING OFF THE GUESS AND PORT/APP NAME IF IT'S PRESENT
+			var portStr string
+			var portNo int
+			if strings.Contains(stream.DstPort, "(") {
+				portStr = strings.Split(stream.DstPort, "(")[0]
+			} else {
+				portStr = stream.DstPort
+			}
+
+			portNo, _ = strconv.Atoi(portStr)
+			if portNo < lowestEphemeralPort {
+				portNumbers.Add(stream.DstPort)
+				fmt.Printf("Adding port %d\n", portNo)
+			} else {
+				fmt.Printf("Dropping port %d\n", portNo)
+			}
+
+		}
+
+		// IF THERE ARE MORE THAN 10 DIFFERENT DESTINATION PORTS FROM A SOURCE HOST, IT'S PROBABLY AN ATTACKER
+		if portNumbers.Size() >= reasonableDifferentPortThreshhold {
+
+			// MARK IT AS AN ATTACKER
+			attackingHosts = append(attackingHosts, srcHost)
+			fmt.Printf("%s scanned the following Ports: %v\n", srcHost, portNumbers.Items())
+
+			// TODO: THIS ASSUMES ALL OF AN ATTACKER'S STREAMS ARE SCANS, WHICH IS PROBABLY NOT THE CASE.
+			// TODO: FILTER OUT NON-ATTACK STREAMS
+			// MAP THE TARGET HOSTS AND PORTS TOGETHER
+			for _, stream := range streams {
+
+				// ONLY GET CONNECT SCAN STREAMS
+				_, exists := targetHostToPortMap[stream.DstHost]
+				if exists {
+					targetHostToPortMap[stream.DstHost].Add(stream.DstPort)
+				} else {
+					targetHostToPortMap[stream.DstHost] = NewSet()
+					targetHostToPortMap[stream.DstHost].Add(stream.DstPort)
+				}
+
+			}
+
+			// REMOVE TARGETS WITH < 10 PORTS IN SCAN SINCE IT'S LIKELY JUST A CONN ERROR
+			for dstHost := range targetHostToPortMap {
+				if targetHostToPortMap[dstHost].Size() < 10 {
+					delete(targetHostToPortMap, dstHost)
+				}
+			}
+		}
+
+	}
+
+	return &attackingHosts, &targetHostToPortMap
 }
 
 // PERFORM ANALYSIS
@@ -162,87 +240,21 @@ func Analyze(pathToPcap string) error {
 			flowLengths = append(flowLengths, (*tcpStreams)[stream].Length)
 		}
 
-		// COUNT PACKETS AND PERCENTAGE OF CONNECTIONS THAT HAVE ONLY A SYN
-		synOnly := 0
-		oneOnly := 0
-		for stream := range *tcpStreams {
-			s := (*tcpStreams)[stream]
-			if s.HasSYN && !s.HasACK {
-				synOnly += 1
-			} else if s.Length == 1 {
-				oneOnly += 1
-			}
-		}
-		//fmt.Printf("%d streams only have a SYN and are therefore probably port scans\n", synOnly)
-		//fmt.Printf("%d additional streams only have one packet, and are therefore probably port scans\n", oneOnly)
-		//fmt.Printf("%f percent of streams are port scans!\n", (float32(synOnly+oneOnly)/float32(len(*tcpStreams)))*100.0)
-
-		// PERFORM TARGET IDENTIFICATION
-		//attackingHosts, victimHosts, victimPorts, victimPortMap := identifyTargets(tcpStreams)
-		//fmt.Printf("Attacking Hosts: %v\n", attackingHosts.Items())
-		//fmt.Printf("Victim hosts: %v\n", victimHosts.Items())
-		//fmt.Printf("Victim Ports: %v\n", victimPorts.Items())
-		//fmt.Printf("Victim Port map: %v\n", victimPortMap)
-
-
-		var attackingHosts []string // LIST OF SRC IPS
-		targetHostToPortMap := make(map[string] *Set)
-
-
+		// CORRELATE TCP STREAMS TO THE ORIGINATING HOST
 		tcpStreamsBySrcHost, err := correlateStreamsToHosts(tcpStreams)
 		if err != nil {
 			return err
 		}
 
-		// IDENTIFY ATTACKS BY ID A HOST IS THE SOURCE OF CONNECTIONS TO > 10 DIFFERENT PORT NUMBERS IN A SHORT AMOUNT OF TIME
-		// THIS WOULD PROBABLY BE A TCP CONNECT SCAN
-		// LOOP ACROSS STREAMS BY SRC HOST
-		for srcHost := range *tcpStreamsBySrcHost {
-			streams := (*tcpStreamsBySrcHost)[srcHost]
+		// IDENTIFY HOSTS THAT TALK TO LOTS OF DIFFERENT PORTS
+		var attackingHosts *[]string // LIST OF SRC IPS
+		var targetHostToPortMap *map[string]*Set
+		attackingHosts, targetHostToPortMap = identifyScans(tcpStreamsBySrcHost)
 
-			// BUILD A LIST OF TARGET PORTS FOR EACH SRC HOST
-			portNumbers := Set{}
-			for _, stream := range streams {
-				portNumbers.Add(stream.DstPort)
-			}
-
-			// IF THERE ARE MORE THAN 10 DIFFERENT DESTINATION PORTS FROM A SOURCE HOST, IT'S PROBABLY AN ATTACKER
-			if portNumbers.Size() >= 10 {
-
-				// MARK IT AS AN ATTACKER
-				attackingHosts = append(attackingHosts, srcHost)
-
-				// TODO: THIS ASSUMES ALL OF AN ATTACKER'S STREAMS ARE SCANS, WHICH IS PROBABLY NOT THE CASE.
-				// TODO: FILTER OUT NON-ATTACK STREAMS
-				// MAP THE TARGET HOSTS AND PORTS TOGETHER
-				for _, stream := range streams {
-
-					// ONLY GET CONNECT SCAN STREAMS
-					if (stream.HasSYN && stream.HasRST) || stream.Length == 1{
-						_, exists := targetHostToPortMap[stream.DstHost]; if exists {
-							targetHostToPortMap[stream.DstHost].Add(stream.DstPort)
-						} else {
-							targetHostToPortMap[stream.DstHost] = NewSet()
-							targetHostToPortMap[stream.DstHost].Add(stream.DstPort)
-						}
-					}
-
-				}
-
-				// REMOVE TARGETS WITH < 10 PORTS IN SCAN SINCE IT'S LIKELY JUST A CONN ERROR
-				for dstHost := range targetHostToPortMap {
-					if targetHostToPortMap[dstHost].Size() < 10 {
-						delete(targetHostToPortMap, dstHost)
-					}
-				}
-			}
-
-
-		}
 		fmt.Printf("Attacking hosts: %v\n", attackingHosts)
 		fmt.Println("Target Host to port map: \n")
-		for targetHost := range targetHostToPortMap {
-			fmt.Printf("%s: %+v\n", targetHost, *(targetHostToPortMap[targetHost]))
+		for targetHost := range *targetHostToPortMap {
+			fmt.Printf("%s: %+v\n", targetHost, (*targetHostToPortMap)[targetHost].Items())
 		}
 		// TODO: PRINT OUTPUT IN A PRETTY FORMAT
 
